53cd1539da9cd48d4852f49caa630b4f8719d2d3
JuneMuoti <june.muoti@gmail.com>
2021-08-06 13:10:45 +0300

    Add flex box

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,14 +1,19 @@
 import Image from "next/image"
 function SmallCard({img,location,distance}) {
     return (
-        <div>
+        <div  className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
             {/* Leftside of the card */}
-            <div className="relative h-16 w-16 grid grid-cols-4" >
+            <div className="relative h-16 w-16" >
                 <Image 
                 src={img}
                 layout="fill"
                 className="rounded-lg"
                 />
+            </div>
+            {/* Right side */}
+            <div>
+                <h2>{location}</h2>
+                <h3 className="text-gray-500">{distance}</h3>
             </div>
     
         </div>
