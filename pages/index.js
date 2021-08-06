0a3b8f8ec615e61c86d65f3fae100a2b21ccfdcc
JuneMuoti <june.muoti@gmail.com>
2021-08-06 13:10:07 +0300

    Add grid

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default function Home({exploreData}) {
     <main className="max-w-7xl mx-auto px-8 sm:px-16">
      <section className="pt-6">
        <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
        {exploreData?.map(item => (
         <SmallCard 
         key={item.img }
@@ -28,6 +29,8 @@ export default function Home({exploreData}) {
         location={item.location}
         />
        ))}
+       </div>
+     
      </section>
     </main>
 
