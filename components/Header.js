9a44a5955a1d479f710369a3db8f5154fda577df
JuneMuoti <june.muoti@gmail.com>
2021-08-07 22:02:39 +0300

    Pass query parameters to search

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,61 @@
 import Image from "next/image";
 import { Input } from "postcss";
+import { useState } from "react";
 import {
     SearchIcon,
     GlobeAltIcon,
     MenuIcon,
     UserCircleIcon,
-    UsersIcon
+    UsersIcon,
+    DeviceTabletIcon
 } from '@heroicons/react/solid'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from "next/dist/client/router";
 
 function Header() {
+    const[searchInput,setSearchInput]= useState("");
+    const[startDate,setStartDate]=useState(new Date());
+    const[endDate,setEndDate]=useState(new Date());
+    const[guestNumber,setGuestNumber]=useState(1);
+    console.log(searchInput);
+    console.log(guestNumber);
+    console.log(startDate);
+   const  handleSelect=(ranges)=>{
+       setStartDate(ranges.selection.startDate);
+       setEndDate(ranges.selection.endDate);
+   }
+
+    
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+      }
+      const resetInput=()=>{
+          setSearchInput("");
+      };
+      const router=useRouter();
+      const search =() =>{
+          router.push({
+              pathname:"/search",
+              query:{
+                location:searchInput,
+                startDate:startDate.toISOString(),
+                endDate:endDate.toISOString(),
+                guestNumber
+              }
+              
+          })
+      }
     return (
        
             <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 p-5 md:px-10">
 
            
             {/* Left Div -Logo */}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={()=> router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image 
                 src="https://links.papareact.com/qd3"
                 layout="fill"
@@ -26,7 +66,7 @@ function Header() {
             </div>
             {/* Middle Div  -Search*/}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-<input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
+<input  value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
 <SearchIcon  className="hidden md:inline-flex  h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
             {/* Right Div */}
@@ -38,7 +78,27 @@ function Header() {
     <UserCircleIcon className="h-6" />
 </div>
             </div>
-       
+       {searchInput &&(
+           <div className="flex flex-col col-span-3 mx-auto mt-3">
+               <DateRangePicker
+               ranges={[selectionRange]}
+               minDate={new Date()}
+               rangeColors={["#FD5B61"]}
+               onChange={handleSelect}
+               
+               />
+               <div className="flex items-center border-b mb-4">
+                   <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+                   <UsersIcon className="h-5" />
+                   <input type="number" value={guestNumber} onChange={(e) => setGuestNumber(e.target.value)} min={1} className="w-12 pl-2 text-lg text-red-400 outline-none " />
+               </div>
+               <div className="flex">
+                   <button className="flex-grow text-gray-500 " onClick={resetInput}>Cancel</button>
+                    <button onClick={search} className="flex-grow text-red-400">Search</button> 
+
+               </div>
+           </div>
+       )}
         </header>
     )
 }
