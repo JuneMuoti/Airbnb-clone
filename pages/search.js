108503a0c7efce1e218b5d85f7b0b4318de27d49
JuneMuoti <june.muoti@gmail.com>
2021-08-07 22:01:52 +0300

    Display location,selected dates and number of guests in search results

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,23 @@
+import { useRouter } from "next/dist/client/router"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import {format} from "date-fns"
 
 function Search() {
+    const router=useRouter();
+    
+     const{location,startDate,endDate,guestNumber}=router.query;
+     const formattedStartDate=format(new Date(startDate),"dd MMM yy");
+     const formattedEndDate=format(new Date(endDate),"dd MMM yy");
+     const range=`${formattedStartDate}-${formattedEndDate}`;
+     console.log(formattedStartDate);
     return (
         <div>
             <Header />
                 <main className="flex">
                     <section className="flex-grow pt-14 px-6">
-                        <p className="text-xs">300+ Stays for 5 number of guests</p>
-                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+                        <p className="text-xs">300+ Stays -{range}  - for {guestNumber} number of guests</p>
+                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
                         <div className="hidden lg:inline-flex mb-5 space-x-3" >
                             <p className="button">Cancellation flexibility</p>
                             <p className="button"> Type of place</p>
