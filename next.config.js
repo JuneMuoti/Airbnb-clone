8b58266f006439c2a7a8a9c948083e52d226f0f1
JuneMuoti <june.muoti@gmail.com>
2021-08-10 11:24:57 +0300

    Added mapbox_key

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['links.papareact.com'],
     },
+      env:{
+        mapbox_key:'pk.eyJ1IjoianVuZW11b3RpIiwiYSI6ImNrczRkbTR0eDIzdTkyeG1zY3VlNTV2b24ifQ.-bw4InBeb0uHK1-L3BM83A'
+      },
   }
